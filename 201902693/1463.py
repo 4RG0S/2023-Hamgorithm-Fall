bc42396bfcbf1fbd544d94d1f091ffae0a0ec2e3
yongbeompark <105450053+yongbeompark@users.noreply.github.com>
2023-09-19 22:53:38 +0900

    1463번 - 1로 만들기

---
+++ 201902693/1463.py
@@ -0,0 +1,17 @@
+def make_one(N):
+    dp = [0] * (N + 1)
+
+    for i in range(2, N + 1):
+        dp[i] = dp[i - 1] + 1
+
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1) 
+
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i // 3] + 1) 
+
+    return dp[N]
+
+N = int(input())
+result = make_one(N)
+print(result)
