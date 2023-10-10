7fc2cdfd7258bedf322e8193483c3df2c7862135
Ori-Gui <dign552@naver.com>
2023-10-11 00:38:04 +0900

    10989번 - 수 정렬하기 3

--- 201902716/2023.10.10_1463.py
+++ 201902716/2023.10.10_1463.py
@@ -1,24 +1,3 @@
-# import sys
-
-# def main():
-#     N = int(sys.stdin.readline().rstrip())
-    
-#     dp = [0] * (N + 1)
-#     dp[1] = 0
-    
-#     for i in range(2, N + 1):
-#         dp[i] = dp[i - 1] + 1
-#         if i % 3 == 0:
-#             dp[i] = min(dp[i], dp[i // 3] + 1)
-#         if i % 2 == 0:
-#             dp[i] = min(dp[i], dp[i // 2] + 1)
-    
-#     print(dp[N])
-
-# if __name__ == "__main__":
-#     main()
-
-
 import sys
 
 def main():
